9e82c1e354de2a3407ac9da24738f252a74f01bb
nnaumovi <70389095+nnaumovi@users.noreply.github.com>
2021-10-03 08:16:00 -0400

    Update data_cleaning.py

--- data_cleaning.py
+++ data_cleaning.py
@@ -1,13 +1,32 @@
 import pandas as pd
 
-df = pd.read_csv('apps.csv', encoding='windows-1252')
-ids = df['id']
-names = df['name']
-keywords = df['keywords'].str.split(', ')
 
-apps = []
-for i in range(0, names.size):
-    keys = []
-    keys.extend(keywords[i][1:])
-    if len(keys) > 1:
-        apps.append({'title': [names[i]], 'keywords': keys[:-2], 'cat': keys[1]})
+def separate():
+    df = pd.read_csv('apps.csv', encoding='windows-1252')
+    ids = df['id']
+    names = df['name']
+    keywords = df['keywords'].str.split(', ')
+
+    apps = []
+    for i in range(0, names.size):
+        keys = []
+        keys.extend(keywords[i][1:])
+        if "Inc." in keys:
+            keys.remove("Inc.")
+        if len(keys) > 1:
+            apps.append({'title': [names[i]], 'keywords': keys[:-2], 'cat': keys[1]})
+
+    dict = {'f1':[], 'f2':[], 'f3':[], 'f4':[], 'f5':[]}
+
+    for i in range(0, 100):
+        dict["f1"].append(apps[i])
+        i += 1
+        dict["f2"].append(apps[i])
+        i += 1
+        dict["f3"].append(apps[i])
+        i += 1
+        dict["f4"].append(apps[i])
+        i += 1
+        dict["f5"].append(apps[i])
+
+    return dict
